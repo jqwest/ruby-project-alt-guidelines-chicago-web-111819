fe33e7dd6c1d9b2e5bf34de6b7a4605e7e11005a
John Leavell <johnleavell@gmail.com>
2020-01-24 10:31:40 -0600

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
-
-  create_table "park_amenities", force: :cascade do |t|
-    t.boolean "jungle_gym"
-    t.boolean "merry_go_round"
-    t.boolean "baseball_field"
-    t.boolean "soccer_field"
-    t.boolean "basketball_court"
-    t.boolean "football_field"
-    t.boolean "arts_and_crafts"
-    t.integer "park_id"
-  end
-
-  create_table "parks", force: :cascade do |t|
-    t.string "name"
-    t.string "city"
-    t.string "state"
-    t.string "address"
-    t.string "opens"
-    t.string "closes"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "user"
-    t.string "review"
-    t.integer "user_id"
-    t.integer "park_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "city"
-    t.string "state"
-    t.string "street_address"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
